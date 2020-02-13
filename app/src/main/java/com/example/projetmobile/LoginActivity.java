7be55d966ec0c2ce6b0ef7faba2364f200c4776f
Chadiii <chadilaoulaou@gmail.com>
2020-02-13 04:57:08 +0100

    Login activity done and connected to FireBase Register activity done and connected to FireBase

--- app/src/main/java/com/example/projetmobile/LoginActivity.java
+++ app/src/main/java/com/example/projetmobile/LoginActivity.java
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                                 startActivity(intent);
 
 
