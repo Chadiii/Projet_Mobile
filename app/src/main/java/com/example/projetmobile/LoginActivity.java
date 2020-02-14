9e158fefe96c72c3c9ff3aa5fe1b79790d830b33
YassineATM <yassine.atmani01@gmail.com>
2020-02-14 11:01:53 +0100

    Yassine : Add navbar, menuuActivity

--- app/src/main/java/com/example/projetmobile/LoginActivity.java
+++ app/src/main/java/com/example/projetmobile/LoginActivity.java
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, menuu.class);
                                 startActivity(intent);
 
 
